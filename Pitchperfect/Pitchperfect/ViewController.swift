ce8238070706f082d35b0638e7e76d2cab9dbad3
Annie Gardner <anniemcihaelwilson@gmail.com>
2021-02-16 12:32:04 -0500

    update

--- Pitchperfect/Pitchperfect/ViewController.swift
+++ Pitchperfect/Pitchperfect/ViewController.swift
@@ -10,12 +10,22 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("View will appear called")
+    }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(<#T##animated: Bool##Bool#>)
+    }
     @IBAction func recordAudio(_ sender: Any) {
         print("record button was pressed")
         recordingLabel.text = "Recording in Progress"
@@ -25,5 +35,7 @@ class ViewController: UIViewController {
         print("recording stopped")
         recordingLabel.text = "Recording Complete"
     }
+
+    
 }
 
